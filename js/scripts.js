d88ea1b4768af1f74f481cfb0e93561758c738d1
Mathew Akre <mwakre29@gmail.com>
2019-04-02 21:41:26 -0700

    3rd passing spec

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     var result = leapYear(year);
     $("#result").text(result);
     var leapYear = function(year) {
-      if (year % 4 === 0) {
+      if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
         return true;
       } else {
         return false;
